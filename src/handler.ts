af7095d505146c2f1462a6c3d38fb87bdea55b97
harryjp95 <harryjp1995@gmail.com>
2020-12-24 15:42:43 +0000

    Add encoding

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import { Context, Handler } from 'aws-lambda'
 
-const html = fs.readFileSync('dist/index.html')
+const html = fs.readFileSync('dist/index.html', 'utf-8')
 
 export const handler: Handler = (_: any, __: Context) => {
   return {
